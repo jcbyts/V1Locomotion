152bb0f5daa548c2e16f8f92d62b5b3a4331bf4f
Jake <jacoby8s@gmail.com>
2022-05-19 13:30:10 -0400

    use SharedLatentGain instead of SharedGain model...

--- Code/fit_latents_session.py
+++ Code/fit_latents_session.py
@@ -1,4 +1,5 @@
 
+from lib2to3.pgen2.token import NT_OFFSET
 import os, sys
 from importlib_metadata import requires
 from matplotlib import use
@@ -22,7 +23,7 @@ from NDNT.training import LBFGSTrainer
 
 from torch.utils.data import Subset, DataLoader
 
-from models import SharedGain
+from models import SharedGain, SharedLatentGain
 
 '''
 Model fitting procedure for the shared gain / offset model
@@ -72,6 +73,97 @@ def rsquared(y, yhat, dfs=None):
     r2 = 1 - ssres/sstot
 
     return r2.detach().cpu()
+
+def censored_lstsq(A, B, M):
+    """Solves least squares problem with missing data in B
+    Note: uses a broadcasted solve for speed.
+    Args
+    ----
+    A (ndarray) : m x r matrix
+    B (ndarray) : m x n matrix
+    M (ndarray) : m x n binary matrix (zeros indicate missing values)
+    Returns
+    -------
+    X (ndarray) : r x n matrix that minimizes norm(M*(AX - B))
+
+    based off code from: http://alexhwilliams.info/itsneuronalblog/2018/02/26/crossval/
+    """
+
+    if A.ndim == 1:
+        A = A[:,None]
+
+    # else solve via tensor representation
+    rhs = (A.T@(M * B)).T[:,:,None] # n x r x 1 tensor
+    T = torch.matmul(A.T[None,:,:], M.T[:,:,None] * A[None,:,:]) # n x r x r tensor
+    try:
+        # transpose to get r x n
+        return torch.squeeze(torch.linalg.solve(T, rhs), dim=-1).T
+    except:
+        r = T.shape[1]
+        T[:,torch.arange(r),torch.arange(r)] += 1e-6
+        return torch.squeeze(torch.linalg.solve(T, rhs), dim=-1).T
+
+
+def pca_train(data, rank, Mtrain, max_iter):
+    
+    # choose solver for alternating minimization
+    solver = censored_lstsq
+
+    # initialize U randomly
+    U = torch.randn(data.shape[0], rank, device=data.device)
+
+    # return U, data, rank, Mtrain
+    Vt = solver(U, data, Mtrain)
+    resid = U@Vt - data
+    mse0 = torch.mean(resid**2)
+    tol = 1e-3
+    # fit pca/nmf
+    for itr in range(max_iter):
+        Vt = solver(U, data, Mtrain)
+        U = solver(Vt.T, data.T, Mtrain.T).T
+        resid = U@Vt - data
+        mse = torch.mean(resid[Mtrain]**2)
+        # print('%d) %.3f' %(itr, mse))
+        if mse > (mse0 - tol):
+            break
+        mse0 = mse
+
+    return mse, U, Vt
+
+def cv_pca(data, rank, Mtrain=None, Mtest=None, p_holdout=0.2, max_iter=10, replicates=5):
+    """Fit PCA while holding out a fraction of the dataset.
+    """
+
+    # create masking matrix
+    if Mtrain is None:
+        Mtrain = torch.rand(*data.shape, device=data.device) > p_holdout
+    
+    if Mtest is None:
+        Mtest = ~Mtrain
+
+    Mtrain = Mtrain.to(data.device)
+    Mtest = Mtest.to(data.device)
+
+    mses = []
+    Us = []
+    Vts = []
+
+    for r in range(replicates):
+        mse, U, Vt = pca_train(data, rank, Mtrain, max_iter)
+        mses.append(mse.item())
+        Us.append(U)
+        Vts.append(Vt)
+    
+    id = np.argmin(np.asarray(mses))
+    U = Us[id]
+    Vt = Vts[id]
+
+    # return result and test/train error
+    resid = U@Vt - data
+    total_err = data - torch.mean(data, dim=0)
+    train_err = 1 - torch.sum(resid[Mtrain]**2) / torch.sum(total_err[Mtrain]**2)
+    test_err = 1 - torch.sum(resid[Mtest]**2) / torch.sum(total_err[Mtest]**2)
+    return U, Vt, train_err, test_err
 
 def fit_model(model, train_dl, val_dl,
     lr=1e-3, max_epochs=5,
@@ -144,6 +236,8 @@ def fit_model(model, train_dl, val_dl,
                     verbose=verbose)
 
             trainer.fit(model, train_dl, val_dl, seed=seed)
+    
+    return trainer
 
 def get_data(fpath, fname, num_tents=10,
         normalize_robs=True,
@@ -201,6 +295,63 @@ def get_data(fpath, fname, num_tents=10,
 
     return ds, dat
 
+def fit_latents(model, mod1, train_dl, fit_sigmas=False, max_iter=10, seed=None):
+
+    if hasattr(model, 'gain_mu'):
+        model.readout_gain.weight.data[:] = 1 #torch.rand(model.readout_gain.weight.shape)
+    
+    if hasattr(model, 'offset_mu'):
+        model.readout_offset.weight.data[:] = 0 #torch.rand(model.readout_offset.weight.shape)
+        
+    # model.gain_mu.weight.data[:] = res2['zgain']
+    # model.gain_mu.weight.data[:] *= 10
+
+    # model.gain_mu.weight_scale = 1.0
+    # model.readout_gain.weight_scale = 1.0
+    # model.readout_gain.weight.data[:] = 1
+
+    model.drift.weight.data = mod1.drift.weight.data[:,model.cids].clone()
+    model.stim.weight.data = mod1.stim.weight.data[:,model.cids].clone()
+    model.bias.data = mod1.bias.data[model.cids].clone()
+
+    model.stim.weight.requires_grad = False
+    model.drift.weight.requires_grad = True
+    model.bias.requires_grad = True
+
+    tol = 1e-6
+    for itr in range(max_iter):
+        if hasattr(model, 'gain_mu'):
+            model.logvar_g.data[:] = 1
+            model.logvar_g.requires_grad = fit_sigmas
+            model.stim.weight.requires_grad = True
+            model.gain_mu.weight.requires_grad = True
+            model.readout_gain.weight.requires_grad = False
+
+        if hasattr(model, 'offset_mu'):
+            model.logvar_h.data[:] = 1
+            model.logvar_h.requires_grad = fit_sigmas
+            model.offset_mu.weight.requires_grad = True
+            model.readout_offset.weight.requires_grad = False
+        
+        t1 = fit_model(model, train_dl, train_dl, use_lbfgs=True, verbose=0, seed=seed)
+
+        if hasattr(model, 'gain_mu'):
+            model.gain_mu.weight.requires_grad = False
+            model.stim.weight.requires_grad = True
+            model.readout_gain.weight.requires_grad = True
+        
+        if hasattr(model, 'offset_mu'):
+            model.offset_mu.weight.requires_grad = False
+            model.readout_offset.weight.requires_grad = True
+        
+        t2 = fit_model(model, train_dl, train_dl, use_lbfgs=True, verbose=0, seed=seed)
+        print('%d: %.3f, %.3f' % (itr, t1.val_loss_min, t2.val_loss_min))
+
+        if t1.val_loss_min - t2.val_loss_min < tol:
+            break
+
+    return t2.val_loss_min, model
+
 def eval_model(mod2, ds, val_ds):
 
     sample = ds[:]
@@ -249,15 +400,50 @@ def eval_model(mod2, ds, val_ds):
 
     return {'rhat': rhat, 'zgain': zg, 'zoffset': zh, 'zgainav': zgav, 'zoffsetav': zhav, 'r2test': r2test}
 
+def fit_gain_model(nstim, mod1, NC=None, NT=None,
+    cids=None, ntents=None, train_dl=None, include_gain=True,
+    include_offset=True, replicates=1): 
+    from copy import deepcopy
+    
+    losses = []
+    models = []
+    for r in range(replicates):
+        mod3 = SharedLatentGain(nstim,
+                    NC=NC,
+                    NT=NT,
+                    cids=cids,
+                    num_latent=1,
+                    num_tents=ntents,
+                    include_stim=True,
+                    include_gain=include_gain,
+                    include_offset=include_offset,
+                    tents_as_input=False,
+                    output_nonlinearity='Identity',
+                    stim_act_func='lin',
+                    stim_reg_vals={'l2':0.01},
+                    reg_vals={'d2t': .01, 'l2': 0.001},
+                    readout_reg_vals={'l2': .1})
+
+        loss, model = fit_latents(mod3, mod1, train_dl, fit_sigmas=True, max_iter=10, seed=None)
+        losses.append(loss)
+        models.append(model)
+        print('Fit run %d: %.3f' % (r, loss))
+
+    id = np.argmin(np.asarray(losses))
+    mod2 = deepcopy(models[id])
+    return mod2
+
 def fit_session(fpath,
         apath,
         fname,
         aname,
-        ntents=5):
+        ntents=5,
+        seed=1234):
 
     device = torch.device("cuda:0" if torch.cuda.is_available() else "cpu")
 
-    seed = 1234
+    figs = [] # initialize list of figures to save into single pdf
+    
     np.random.seed(seed)
     torch.manual_seed(seed)
     torch.cuda.manual_seed(seed)
@@ -281,17 +467,16 @@ def fit_session(fpath,
     TC = sample['stim'].T @ sample['robs']
     TC = (TC.T / sample['stim'].sum(dim=0)).T
 
-    plt.figure(figsize=(10,10))
+    figs.append(plt.figure(figsize=(10,10)))
     for cc in range(NC):
         plt.subplot(sx, sy, cc+1)
         tc = TC[:,cc].detach().cpu().numpy()
         f = plt.plot(directions, np.reshape(tc, [nfreq, ndir]).T, '-o')
-    plt.show()
 
     '''
     Build Train / Test sets
     '''
-    train_dl, val_dl, indices = get_dataloaders(ds, batch_size=64)
+    train_dl, val_dl, indices = get_dataloaders(ds, batch_size=64, use_dropout=True)
     train_inds = indices[0]
     val_inds = indices[1]
 
@@ -308,15 +493,14 @@ def fit_session(fpath,
             include_offset=False,
             tents_as_input=False,
             output_nonlinearity='Identity',
+            latent_noise=True,
             stim_act_func='lin',
-            stim_reg_vals={'l2':0.00},
-            reg_vals={'l2':0.00},
+            stim_reg_vals={'l2':0.01},
+            reg_vals={'l2':0.01},
             act_func='lin')
 
-    mod0.prepare_regularization()
-
     print("Fitting baseline model")
-    fit_model(mod0, train_dl, val_dl, verbose=0, use_lbfgs=True)
+    t1 = fit_model(mod0, train_dl, val_dl, use_lbfgs=True, verbose=0)
     print("Done")
 
     '''
@@ -334,12 +518,11 @@ def fit_session(fpath,
             tents_as_input=False,
             output_nonlinearity='Identity',
             stim_act_func='lin',
-            stim_reg_vals={'l2':0.01},
-            reg_vals={'l2':1},
+            stim_reg_vals={'l2':1},
+            reg_vals={'l2':0.001},
             act_func='lin')
 
     mod1.drift.weight.data = mod0.drift.weight.data.clone()
-    mod1.prepare_regularization()
 
     print("Fitting Stimulus Model")
     fit_model(mod1, train_dl, val_dl, verbose=0, use_lbfgs=True)
@@ -348,111 +531,49 @@ def fit_session(fpath,
     res0 = eval_model(mod0, ds, val_dl.dataset)
     res1 = eval_model(mod1, ds, val_dl.dataset)
 
-    plt.figure()
+    figs.append(plt.figure())
     plt.plot(res0['r2test'], 'o', label='Baseline')
     plt.plot(res1['r2test'], 'o', label='Stimulus')
     plt.axhline(0, color='k')
     plt.ylabel('$r^2$')
     plt.xlabel("Unit ID")
     plt.ylim([-0.1,1])
+    plt.legend()
+    
 
     '''
     Affine model
     '''
-    cids = np.where(np.logical_and(res1['r2test'] > res0['r2test'], res1['r2test'] > 0))[0]
-
-    mod2 = SharedGain(nstim,
-                NC=NC,
-                cids=cids,
-                num_latent=1,
-                num_tents=ntents,
-                include_stim=True,
-                include_gain=True,
-                include_offset=True,
-                tents_as_input=False,
-                output_nonlinearity='Identity',
-                stim_act_func='lin',
-                stim_reg_vals={'l2':0.01},
-                reg_vals={'l2':1},
-                act_func='lin')
-
-    mod2.drift.weight.data = mod1.drift.weight.data[:,cids].clone()
-    mod2.stim.weight.data = mod1.stim.weight.data[:,cids].clone()
-    mod2.bias.data = mod1.bias.data[cids].clone()
-    mod2.drift.weight.requires_grad = False # individual neuron drift is fixed
-    mod2.stim.weight.requires_grad = True # stimulus fit at same time as gain
-    mod2.bias.requires_grad = True
-
-    mod2.prepare_regularization()
-
+    # cids = np.where(np.logical_and(res1['r2test'] > res0['r2test'], res1['r2test'] > 0))[0]
+    cids = np.where(res1['r2test'] > res0['r2test'])[0]
+    print("Found %d /%d units with significant stimulus effect" %(len(cids), len(res1['r2test'])))
+    
     print("Fitting Affine Model")
-    fit_model(mod2, train_dl, val_dl, verbose=0, use_lbfgs=True)
+    mod2 = fit_gain_model(nstim, mod1, NC=NC, NT=len(ds),
+    cids=cids, ntents=ntents, train_dl=train_dl, include_gain=True,
+    include_offset=True)
+
     print("Done")
 
     ''' 
     Gain Only
     '''
-    mod3 = SharedGain(nstim,
-                NC=NC,
-                cids=cids,
-                num_latent=1,
-                num_tents=ntents,
-                include_stim=True,
-                include_gain=True,
-                include_offset=False,
-                tents_as_input=False,
-                output_nonlinearity='Identity',
-                stim_act_func='lin',
-                stim_reg_vals={'l2':0.01},
-                reg_vals={'l2':1},
-                act_func='lin')
-
-    mod3.drift.weight.data = mod2.drift.weight.data.clone()
-    mod3.stim.weight.data = mod2.stim.weight.data.clone()
-    mod3.bias.data = mod2.bias.data.clone()
-    mod3.latent_gain.weight.data = mod2.latent_gain.weight.data.clone()
-    mod3.readout_gain.weight.data = mod2.readout_gain.weight.data.clone()
-    mod3.drift.weight.requires_grad = True
-    mod3.stim.weight.requires_grad = True
-    mod3.bias.requires_grad = True
-
-    mod3.prepare_regularization()
-
     print("Fitting Gain Model")
-    fit_model(mod3, train_dl, val_dl, verbose=0, use_lbfgs=True)
+    mod3 = fit_gain_model(nstim, mod1, NC=NC, NT=len(ds),
+    cids=cids, ntents=ntents, train_dl=train_dl, include_gain=True,
+    include_offset=False)
+
     print("Done")
 
     ''' 
     Offset Only
     '''
-    mod4 = SharedGain(nstim,
-                NC=NC,
-                cids=cids,
-                num_latent=1,
-                num_tents=ntents,
-                include_stim=True,
-                include_gain=False,
-                include_offset=True,
-                tents_as_input=False,
-                output_nonlinearity='Identity',
-                stim_act_func='lin',
-                stim_reg_vals={'l2':0.01},
-                reg_vals={'l2':1},
-                act_func='lin')
-
-    mod4.drift.weight.data = mod2.drift.weight.data.clone()
-    mod4.stim.weight.data = mod2.stim.weight.data.clone()
-    mod4.bias.data = mod2.bias.data.clone()
-    mod4.latent_offset.weight.data = mod2.latent_offset.weight.data.clone()
-    mod4.readout_offset.weight.data = mod2.readout_offset.weight.data.clone()
-    mod4.drift.weight.requires_grad = False
-    mod4.stim.weight.requires_grad = True
-    mod4.bias.requires_grad = True
-
-    mod4.prepare_regularization()
-
+    
     print("Fitting Offset Model")
-    fit_model(mod4, train_dl, val_dl, verbose=0, use_lbfgs=True)
+    mod4 =  fit_gain_model(nstim, mod1, NC=NC, NT=len(ds),
+    cids=cids, ntents=ntents, train_dl=train_dl, include_gain=False,
+    include_offset=True)
+
     print("Done")
 
     sample = ds[:]
@@ -505,10 +626,56 @@ def fit_session(fpath,
     moddict4['model'] = mod4
     das['offset'] = moddict4
 
+    print("Fitting CV PCA")
+    ##%% redo PCA on fit neurons
+    das['cvpca'] = []
+    train_err= []
+    test_err = []
+    ranks = range(1, 25)
+    for rnk in ranks:
+        data = ds.covariates['robs'][:,cids]
+        Mtrain = train_dl.dataset[:]['dfs'][:,cids]>0
+        Mtest = val_dl.dataset[:]['dfs'][:,cids]>0
+
+        U, Vt, tre, te = cv_pca(data, rank=rnk, Mtrain=Mtrain, Mtest=Mtest)
+        
+        resid = U@Vt - data
+        mu = torch.sum(data*Mtrain, dim=0)/torch.sum(Mtrain, dim=0)
+
+        total_err = data - mu
+
+        tre = 1 - torch.sum(resid**2*Mtrain, dim=0) / torch.sum(total_err**2*Mtrain, dim=0)
+        te = 1 - torch.sum(resid**2*Mtest, dim=0) / torch.sum(total_err**2*Mtest, dim=0)
+
+        das['cvpca'].append({'rank': rnk, 'U': U.cpu().numpy(), 'Vt': Vt.cpu().numpy(), 'r2train': tre.cpu().numpy(), 'r2test': te.cpu().numpy()})
+        train_err.append((rnk, tre.mean().item()))
+        test_err.append((rnk, te.mean().item()))
+        
+    
+
     print("Saving...")
     with open(os.path.join(apath, aname), 'wb') as f:
         pickle.dump(das, f)
     print("Done")
+
+    ## %% Compare PCA to latent model
+    fig, ax = plt.subplots(1, 1, figsize=(8, 4.5))
+    ax.plot(*list(zip(*train_err)), 'o-b', label='PCA Train Data')
+    ax.plot(*list(zip(*test_err)), 'o-r', label='PCA Test Data')
+    ax.plot(1, das['stimdrift']['r2test'].mean(), 'o', label='Stim Model', color='k')
+    ax.plot(1, das['gain']['r2test'].mean(), 'o', label='1 Gain Latent Model', color='m')
+    ax.axhline(das['gain']['r2test'].mean(),color='m')
+    # ax.plot(1, res2['r2test'].mean(), 'o', label='1 Gain Autoencoder Model', color='g')
+    ax.set_ylabel('Var. Explained')
+    ax.set_xlabel('Number of PCs')
+    ax.set_title('PCA vs. 1D Shared Gain')
+    ax.spines['top'].set_visible(False)
+    ax.spines['right'].set_visible(False)
+    ax.legend()
+    plt.ylim(0, 1)
+    fig.tight_layout()
+
+    
 
     return das
     
