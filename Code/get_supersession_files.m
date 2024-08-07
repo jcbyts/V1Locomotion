313be2c2f8f90f4d7219174372d0be66e19df0ec
Jacob Yates <jacoby8s@gmail.com>
2024-08-06 21:27:40 -0700

    edited super session import for figshare data

--- Code/get_supersession_files.m
+++ Code/get_supersession_files.m
@@ -6,7 +6,7 @@ fdir = fullfile(fpath, 'gratings');
 
 fprintf('Building supersession files. This can be slow...\n')
 
-subjs = {'brie', 'gru'};
+subjs = {'brie', 'gru', 'allen'};
 for isubj = 1:numel(subjs)
 
     subj = subjs{isubj};
@@ -20,12 +20,12 @@ end
 fprintf('\n\nDone with marmosets\n\n')
 
 %% Import for the Allen Institute data
-% If the data is not already in the path, run allen_data_to_matlab.py
-fprintf('Building supersession struct for the Allen Institute data\n')
-fdir = fullfile(fpath, 'brain_observatory_1.1');
-subj = 'allen';
-import_supersession(subj, fdir)
-
-% move file over
-fname = sprintf('%sD_all.mat', subj);
-movefile(fullfile(fdir, fname), fullfile(fpath, fname))
+% If the data is not already in the grating path, run allen_data_to_matlab.py
+% fprintf('Building supersession struct for the Allen Institute data\n')
+% fdir = fullfile(fpath, 'brain_observatory_1.1');
+% subj = 'allen';
+% import_supersession(subj, fdir)
+% 
+% % move file over
+% fname = sprintf('%sD_all.mat', subj);
+% movefile(fullfile(fdir, fname), fullfile(fpath, fname))
